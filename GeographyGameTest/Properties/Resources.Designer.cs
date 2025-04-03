146eb873cf2c584bad367bcfc1d0391b4d4833ca
Smilla Hallgren Larsson <smilla.hallgren-larsson1@yaelev.se>
2025-04-03 16:45:35 +0200

    name change

--- GeographyGameTest/Properties/Resources.Designer.cs
+++ GeographyGameTest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GeographyGameTest.Properties {
+namespace GeoGame.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GeographyGameTest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GeographyGameTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GeoGame.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
